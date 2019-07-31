8a674ff2544528b6de96e0d5dd2e4b1a7c1c5084
Allen Tools <email@allen.tools>
2019-07-31 11:20:13 -0400

    Update files

--- vps_setup.sh
+++ vps_setup.sh
@@ -6,31 +6,39 @@ sudo apt-get install -y mysql-server
 # Disable table access
 sudo echo "skip-grant-tables" >> /etc/mysql/my.cnf
 
+# Set new password
+read -s -p "Enter Password: " password
+mysqladmin --user=root password $password
+
+# Remove disable table access
+sudo sed '$d' /etc/mysql/my.cnf
+
+# OR USE MySQL CONSOLE
+# Start, remove privileges, set new password
+# sudo /etc/init.d/mysql stop
+# sudo /usr/sbin/mysqld --skip-grant-tables --skip-networking &
+# mysql -u root
+# FLUSH PRIVILEGES;
+# update mysql.user set password=password('password") where user='root';
+# FLUSH PRIVILEGES;
+# exit
+
 # Restart server
 sudo /etc/init.d/mysql restart
 sudo /etc/init.d/mysql stop
 sudo /etc/init.d/mysql start
 
-# Set new password
-read -s -p "Enter Password: " password
-mysqladmin --user=root password $password
-
-# OR USE MySQL CONSOLE
-# Start, remove privileges, set new password
-# mysql -u root -p 
-  # use at mysql> flush privileges;
-  # ALTER USER 'root'@'localhost'  IDENTIFIED BY 'NewPassword';
-
-# Remove disable table access
-sudo sed '$d' /etc/mysql/my.cnf
-
 # Open a prompt
-echo "Open a prompt with 'mysql -p'"
+echo "Open a prompt with 'sudo mysql -u root -ppassword'"
+
+# Install and configure Git, apache, pip
+sudo apt-get install python-pip git apache2 libapache2-mod-wsgi
+sudo chown -R $USER /var/www
 
 # Python drivers for MySQL
 pip install --user pymysql
 
-# Clone
+# Clone Repo
 cd /var/www
 git clone https://github.com/kayfay/python-flask-crime-map.git
 cd python-flask-crime-map
@@ -38,7 +46,7 @@ cd python-flask-crime-map
 # Add credentials
 cat <<EOF > dbconfig.py
 db_user = "root"
-db_password = "NewPassword"
+db_password = "password"
 EOF
 
 # Credit .gitignore
